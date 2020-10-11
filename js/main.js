91e012d5417b25040e2732a9574da5c606459afe
Vokhidov <vokhidov610@gmail.com>
2020-10-11 16:51:01 +0500

    some changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     dots: false,
-    items:1,
-    loop:true,
+    items: 1,
+    loop: true,
     autoplay: true,
     singleItem: true,
     autoplayTimeout: 3000,
-    autoplayHoverPause: true,  
-    nav:true,
+    autoplayHoverPause: true,
+    nav: true,
     navText: [
       "<button class='carousel-icon'><i class='icon-left-open'></i></button>",
       "<button class='carousel-icon'><i class='icon-right-open'></i></button>",
@@ -15,16 +15,27 @@ $(document).ready(function () {
   });
 });
 
-$('.international-carousel').slick({
+$(".international-carousel").slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   autoplay: true,
-  accessibility:false,
+  accessibility: false,
   autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+      },
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
 });
 
-
-/ PagePiling /
+/ PagePiling /;
 // $(document).ready(function() {
 // 	$('#pagepiling').pagepiling();
 // });
